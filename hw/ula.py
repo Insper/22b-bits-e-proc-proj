0bf4e53ca7989169d9645c62e649ff1566d9258c
c0d8 <matheusaguiar362@gmail.com>
2022-09-19 14:57:34 -0300

    Teste ula

--- hw/ula.py
+++ hw/ula.py
@@ -6,14 +6,16 @@ from myhdl import *
 @block
 def ula(x, y, c, zr, ng, saida, width=16):
 
-    zx_out = Signal(intbv(0)[width:])
-    nx_out = Signal(intbv(0)[width:])
-    zy_out = Signal(intbv(0)[width:])
-    ny_out = Signal(intbv(0)[width:])
-    and_out = Signal(intbv(0)[width:])
-    add_out = Signal(intbv(0)[width:])
-    mux_out = Signal(intbv(0)[width:])
-    no_out = Signal(intbv(0)[width:])
+    zx_out = Signal(modbv(0)[width:])
+    nx_out = Signal(modbv(0)[width:])
+    zy_out = Signal(modbv(0)[width:])
+    ny_out = Signal(modbv(0)[width:])
+    and_out = Signal(modbv(0)[width:])
+    add_out = Signal(modbv(0)[width:])
+    mux_out = Signal(modbv(0)[width:])
+    no_out = Signal(modbv(0)[width:])
+    ng_out = Signal(modbv(0)[width:])
+    zr_out = Signal(modbv(0)[width:])
 
     c_zx = c(5)
     c_nx = c(4)
@@ -22,15 +24,36 @@ def ula(x, y, c, zr, ng, saida, width=16):
     c_f = c(1)
     c_no = c(0)
 
+    z1 = zerador(c_zx, x, zx_out)
+    z2 = zerador(c_zy, y, zy_out)
+    i1 = inversor(c_nx, zx_out, nx_out)
+    i2 = inversor(c_ny, zy_out, ny_out)
+    add1 = add(nx_out, ny_out, add_out)
+    
+    i3 = inversor(c_no, mux_out, no_out)
+    c3 = comparador(no_out, zr_out, ng_out, width)
+
     @always_comb
     def comb():
-        pass
+
+        if int(c_f) == 0:
+            mux_out.next = (nx_out & ny_out)
+        else:
+            mux_out.next = add_out
+
+        saida.next = no_out
+        zr.next = zr_out
+        ng.next = ng_out
+        #print(bin(no_out, 16))
+        #print(bin(ng_out, 1))
+
 
     return instances()
 
 
 # -z faz complemento de dois
 # ~z inverte bit a bit
+
 @block
 def inversor(z, a, y):
     @always_comb
@@ -48,7 +71,17 @@ def comparador(a, zr, ng, width):
     # width insica o tamanho do vetor a
     @always_comb
     def comb():
-        pass
+        print(bin(a, 16))
+        if a[width-1] == 0:
+            ng.next = 0
+        else:
+            ng.next = 1
+
+        if a == 0:
+            zr.next = 1
+        else:
+            zr.next = 0
+    
 
     return instances()
 
@@ -79,13 +112,12 @@ def add(a, b, q):
 
 
 @block
-def inc(a, q1):
- 
+def inc(a, q):
     @always_comb
     def comb():
 
 
-        q1.next = a + 1
+        q.next = a+1
 
     return instances()
 
@@ -152,7 +184,11 @@ def addcla16(a, b, q):
 @block
 def ula_new(x, y, c, zr, ng, sr, sf, bcd, saida, width=16):
 
+
+    if zr[0][0] == 1 :
+        x.next == 0
     
+
 
 
 

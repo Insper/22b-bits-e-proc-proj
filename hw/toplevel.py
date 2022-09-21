28d2ebc50519375e5ded6b652a5ba4f0711ae636
PedroPertusi <pedrovazpertusi@gmail.com>
2022-09-21 09:20:35 -0300

    fpga funcionando

--- hw/toplevel.py
+++ hw/toplevel.py
@@ -3,28 +3,27 @@
 from myhdl import *
 
 from components import *
+from ula import *
 
 
 @block
-def toplevel(LEDR, SW, KEY, hex0, hex1, HEX2, HEX3, HEX4, HEX5, CLOCK_50, RESET_N):
+def toplevel(LEDR, SW, KEY, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, CLOCK_50, RESET_N):
     sw_s = [SW(i) for i in range(10)]
     key_s = [KEY(i) for i in range(10)]
     ledr_s = [Signal(bool(0)) for i in range(10)]
-
-    bc0 = Signal(intbv(0)[4:])
-    bc1 = Signal(intbv(0)[4:])
-
-    ic1 = bin2bcd(SW, bc1, bc0)
-    ihex1 = bin2hex(hex1, bc1)
-    ihex0 = bin2hex(hex0, bc0)
-    #ihex1 = bin2hex(hex0, bc1)
-    #ihex0 = bin2hex(hex1, bc0)
+    saida = Signal(modbv(0)[8:])
+    
+    # x = Signal(intbv(1)[8:])
+    # y = Signal(intbv(2)[8:])
+    ula_1 = ula(1, 2, SW, ledr_s[8], ledr_s[9], saida, width=8)
 
     # ---------------------------------------- #
     @always_comb
     def comb():
-        for i in range(len(ledr_s)):
-            LEDR[i].next = ledr_s[i]
+        for i in range(len(saida)):
+            LEDR[i].next = saida[i]
+        LEDR[8].next = ledr_s[8]
+        LEDR[9].next = ledr_s[9]
 
     return instances()
 

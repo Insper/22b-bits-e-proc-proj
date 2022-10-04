7b962c9d45126ee535c086f01e1827a46c84351b
Eduardosmb <you@example.com>
2022-10-04 17:06:21 -0300

    register8

--- hw/seq.py
+++ hw/seq.py
@@ -46,9 +46,13 @@ def register8(i, load, output, clk, rst):
     binaryDigitList = [None for n in range(8)]
     output_n = [Signal(bool(0)) for n in range(8)]
 
+    for k in range(8):
+        binaryDigitList[k] = binaryDigit(i(k), load, output_n[k], clk, rst)
+
     @always_comb
     def comb():
-        pass
+        for k in range(8):
+            output.next[k] = output_n[k]
 
     return instances()
 
